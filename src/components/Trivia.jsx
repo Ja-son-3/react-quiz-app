3f4b48724860e841aa6f40be40acb9bbee156ffc
Jason Huang <jason97521@gmail.com>
2023-03-08 21:53:32 -0500

    creating quic app logics

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,13 +1,33 @@
-export default function Trivia() {
-  return (
-    <div className="trivia">
-        <div className="question">What's the best youtube channel?</div>
-        <div className="answers">
-            <div className="answer">Lama Dev</div>
-            <div className="answer">Lama Dev</div>
-            <div className="answer">Lama Dev</div>
-            <div className="answer">Lama Dev</div>
+import { useEffect, useState } from "react"
+
+export default function Trivia({
+    data,
+    setTimeOut,
+    questionNumber,
+    setQuestionNumber
+}) {
+    const [question, setQuestion] = useState(null)
+    const [selectedAnswer, setSelectedAnswer] = useState(null)
+    const [className, setClassName] = useState("answer")
+
+    useEffect(()=>{
+        setQuestion(data[questionNumber - 1])
+    },[data, questionNumber])
+
+    const handleClick = (a) =>{
+        setSelectedAnswer(a)
+        setClassName("answer active")
+        set
+    }
+
+    return (
+        <div className="trivia">
+            <div className="question">{question?.question}</div>
+            <div className="answers">
+                {question?.answers.map(a=>(
+                    <div className={selectedAnswer === a ? className: "answer"} onClick={()=>handleClick(a)}>{a.text}</div>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
